4ae1ad01b7feac05d8ddcb2236418482cdfbb1cd
tri-interview <none@none.com>
2025-08-02 10:23:56 +0800

    added support for javascript

--- src/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface ErrorMessageProps {
-  message: string;
-}
-
-export function ErrorMessage({ message }: ErrorMessageProps) {
-  return <p style={{color: 'red', textAlign: 'center', marginTop: '15px'}}>{message}</p>;
-}
--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { Button } from './Button';
-export { Card } from './Card';
-export { Container } from './Container';
-export { ErrorMessage } from './ErrorMessage';
-export { Input } from './Input';
-export { QuestionCard } from './QuestionCard';
-export { ScoreDisplay } from './ScoreDisplay';
-export { Table, TableRow, TableCell, TableHeader } from './Table';
